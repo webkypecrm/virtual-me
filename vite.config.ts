d673a3422442c03b24095187bcca6eba501f24de
webkype <webkypecrm@gmail.com>
2025-08-27 17:50:08 +0530

    first-final-commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/react/template/',
+  base: "/",
   resolve: {
-   alias: {
-     moment: 'moment/moment.js'
-   },
- },
-
-})
+    alias: {
+      moment: "moment/moment.js",
+    },
+  },
+});
